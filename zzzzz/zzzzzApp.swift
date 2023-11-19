79900a359065d7799dc5923092585b41074b316e
fengwmi <1980385456@qq.com>
2023-11-19 20:15:41 +0800

    Initial Commit

---
+++ zzzzz/zzzzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  zzzzzApp.swift
+//  zzzzz
+//
+//  Created by wmi feng on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct zzzzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
